f84692a3415ba94b49a85bb8b75f10f2078b9509
Matthew Kendon <mkendon@gmail.com>
2016-03-02 23:01:25 +0000

    assert that i can get a post from another one through a connection

--- tests/PostWithConnectionTest.php
+++ tests/PostWithConnectionTest.php
@@ -27,4 +27,11 @@ class PostWithConnectionTest extends PHPUnit_Framework_TestCase
         $connection = $post->connections()->type('post_page')->first();
         $this->assertEquals('post_page', $connection->p2p_type);
     }
+
+    public function it_can_get_a_connected_post_from_a_connection()
+    {
+        $post = PostWithConnection::find(1);
+        $connection = $post->connections()->first();
+        $this->assertInstanceOf('\Corcel\Post', $connection->to);
+    }
 }
